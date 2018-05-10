0243ca16e3e4e4aae95f7007cecf538409a3438f
MaximilianoCabrera <maximilianocabrerap@gmail.com>
2018-05-10 14:17:57 -0300

    Fin Video Parte 8
    Hubo error de subida, salta del 5 al 8

---
+++ Lands/Lands/ViewModels/LoginViewModel.cs
@@ -0,0 +1,51 @@
+﻿namespace Lands.ViewModels
+{
+    using System.Windows.Input;
+
+    public class LoginViewModel
+    {
+        #region MyProperties
+        public string Email
+        {
+            get;
+            set;
+        }
+        public string Password
+        {
+            get;
+            set;
+        }
+        public bool IsRunning
+        {
+            get;
+            set;
+        }
+        public bool IsRemember
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        #region Commands
+        public ICommand LoginCommand
+        {
+            get;
+            set;
+        }
+
+        public ICommand RegisterCommand
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        #region Constructors
+        public LoginViewModel()
+        {
+            this.IsRemember = true;
+        }
+        #endregion
+    }
+}
